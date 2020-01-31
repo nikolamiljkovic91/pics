e9ec855e6aed68ff362d3fa799dcffbd7e9730fa
nikolamiljkovic91 <nikolamiljkovic91@gmail.com>
2020-01-31 22:49:26 +0100

    minor css fix

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Header from './components/Header/Header';
 import GridList from './components/Grid/GridList';
 import Footer from './components/Footer/Footer';
 import List from './components/List/List';
+import classes from './App.module.scss';
 
 
 let App = () => {
   return (
-    <>
+    <div className={classes.Wrapper}>
       <Header />
       <GridList />
       <List />
       <Footer />
-    </>
+    </div>
   )
 }
 
