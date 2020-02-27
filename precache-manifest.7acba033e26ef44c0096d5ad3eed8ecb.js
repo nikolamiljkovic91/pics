400d6d03972b76a2271884116f91dc4a428e5b3c
nikolamiljkovic91 <nikolamiljkovic91@gmail.com>
2020-02-27 19:38:54 +0100

    Updates

---
+++ precache-manifest.7acba033e26ef44c0096d5ad3eed8ecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b957638a15576220620b0062753b8259",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "6f85c4f109b942620370",
+    "url": "/pics/static/css/main.ee204514.chunk.css"
+  },
+  {
+    "revision": "07f2f0cf4fde01779819",
+    "url": "/pics/static/js/2.52d4b9a4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pics/static/js/2.52d4b9a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f85c4f109b942620370",
+    "url": "/pics/static/js/main.c591c8cf.chunk.js"
+  },
+  {
+    "revision": "dc8ca64b24fc1af11a9a",
+    "url": "/pics/static/js/runtime-main.e0be9e58.js"
+  }
+]);
